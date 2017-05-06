b723ac9a16b88981cf192808050bef3c9fc9500a
Jorge Rafael <jorgerafael@uxer.com.br>
2017-05-06 14:41:05 -0300

    Update: Simple Website in Modules

--- code/examples/simple-website-in-module/routes.js
+++ code/examples/simple-website-in-module/routes.js
@@ -1,69 +1,82 @@
+(() => {
 
-const filesystem = require('fs');
-const path = require('path');
+	'use strict';
+
+	const filesystem = require('fs');
+	const path = require('path');
 
 
-const onLoad = (page, callback) => {
+	const onLoad = (page, callback) => {
 
-	var result = {}
+		var result = {}
 
-	/** Comente aqui */
-	const onReadFile = (err, data) => {
+		/** Comente aqui */
+		const onReadFile = (err, data) => {
 
-		if (err) {
-			return callback(err, null)
+			if (err) {
+				return callback(err, null)
+			}
+
+			result.data = data;
+
+			return callback(null, result);
 		}
 
-		result.data = data;
+		let file = null;
 
-		return callback(null, result);
-	}
-
-	let file = null;
-
-	/** Comente aqui */
-	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types#textcss
-
-	result.content_type = "text/html";
-
-	if (page.indexOf(".css") > 0) {
-		result.content_type = "text/css";
-	}
-	if (page.indexOf(".js") > 0) {
+		/** Comente aqui */
+		// https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types#textcss
 		result.content_type = "text/html";
+
+		/** Comente aqui */
+		if (page.indexOf(".css") > 0) {
+			result.content_type = "text/css";
+		}
+
+		/** Comente aqui */
+		if (page.indexOf(".js") > 0) {
+			result.content_type = "text/html";
+		}
+
+		/** Comente aqui */
+		result.status = 200;
+
+		/** Comente aqui */
+		switch (page) {
+
+			case "/":
+				/** Comente aqui */
+				file = path.join(__dirname, '../../../files/htmls/hello-world.html');
+				filesystem.readFile(file, onReadFile)
+				break;
+
+			case "/style.css":
+				/** Comente aqui */
+				file = path.join(__dirname, '../../../files/htmls/style.css');
+				filesystem.readFile(file, onReadFile)
+				break;
+
+			case "/scripts.js":
+				/** Comente aqui */
+				file = path.join(__dirname, '../../../files/htmls/scripts.js');
+				filesystem.readFile(file, onReadFile)
+				break;
+
+			default:
+				result.status = 404;
+				/** Comente aqui */
+				file = path.join(__dirname, '../../../files/htmls/404.html');
+				filesystem.readFile(file, onReadFile);
+				break;
+
+		}
+
+
 	}
 
-	result.status = 200;
 	/** Comente aqui */
-	switch (page) {
-		case "/":
-			/** Comente aqui */
-			file = path.join(__dirname, '../../../files/htmls/hello-world.html');
-			filesystem.readFile(file, onReadFile)
-			break;
-		case "/style.css":
-			/** Comente aqui */
-			file = path.join(__dirname, '../../../files/htmls/style.css');
-			filesystem.readFile(file, onReadFile)
-			break;
-		case "/scripts.js":
-			/** Comente aqui */
-			file = path.join(__dirname, '../../../files/htmls/scripts.js');
-			filesystem.readFile(file, onReadFile)
-			break;
-		default:
-			result.status = 404;
-			/** Comente aqui */
-			file = path.join(__dirname, '../../../files/htmls/404.html');
-			filesystem.readFile(file, onReadFile);
-			break;
-
+	module.exports = {
+		Load: onLoad
 	}
 
-
-}
-
-
-module.exports = {
-	Load: onLoad
-}
+})();
